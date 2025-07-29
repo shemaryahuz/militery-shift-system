81e9466a91c74661002eef7891e6800b02b8493a
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-29 11:59:04 +0300

    add Assignments and Shifts modules to the application

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { ShiftsModule } from './shifts/shifts.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, AssignmentsModule, ShiftsModule],
   controllers: [AppController],
   providers: [AppService],
 })
