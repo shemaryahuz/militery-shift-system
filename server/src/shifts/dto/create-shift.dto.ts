c2b334f1e9aed2b5e2d9717b4852c1cd840d7bd3
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-30 15:30:06 +0300

    add error handling to users and shifts controllers

--- server/src/shifts/dto/create-shift.dto.ts
+++ server/src/shifts/dto/create-shift.dto.ts
@@ -2,12 +2,12 @@ import { IsNotEmpty, IsString } from "class-validator";
 import { Transform } from "class-transformer";
 
 export class CreateShiftDto {
-    // expected format: YYYY-MM-DD HH:mm:ss or YYYY-MM-DD HH:mm
+    // expected format: 'YYYY-MM-DD HH:mm:ss' or 'YYYY-MM-DD HH:mm'
     @IsNotEmpty()
     @Transform(({ value }) => new Date(value))
     startTime: Date;
     
-    // expected format: YYYY-MM-DD HH:mm:ss or YYYY-MM-DD HH:mm
+    // expected format: 'YYYY-MM-DD HH:mm:ss' or 'YYYY-MM-DD HH:mm'
     @IsNotEmpty()
     @Transform(({ value }) => new Date(value))
     endTime: Date;
