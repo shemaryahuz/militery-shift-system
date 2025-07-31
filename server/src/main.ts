0f4a4ee402a57e006875fceb1bd47b9bf5b6f26d
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-31 12:53:16 +0300

    update README.md

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
+    forbidNonWhitelisted: true
   }));
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
